5f640d2ab799d8ea9d953ceee2022558aa1dd8f6
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-08-23 21:13:02 +0000

    express setting up server + img

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.use(function(req, res, next) {
+	console.log(`${req.method} request for ${req.url}`);
+	next();
+});
+
+app.use(express.static('./public'));
+
+// app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/about', function(req, res) {
+	res.sendFile(__dirname + '/public/about.html');
+});
+
+// app.post()
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function() {
+	console.log(`Server is running on port ${app.get('port')}`);
+});
