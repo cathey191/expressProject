e4365d0a516efbb816bd734d44961af376172828
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-08-23 23:00:06 +0000

    setup twitter

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const path = require('path');
+const Twitter = require('twitter');
+const config = require('./config');
 const app = express();
+
+var client = new Twitter({
+	TConsumerKey: 'config[0].TConsumerKey',
+	TConsumerKeySecret: 'config[0].TConsumerKeySecret',
+	TAccessToken: 'config[0].TAccessToken',
+	TAccessTokenSecret: 'config[0].TAccessTokenSecret'
+});
 
 app.use(function(req, res, next) {
 	console.log(`${req.method} request for ${req.url}`);
@@ -9,14 +18,12 @@ app.use(function(req, res, next) {
 
 app.use(express.static('./public'));
 app.use(
-	'/bootstrapStyle',
-	express.static(
-		path.join(__dirname, 'node_modules/bootstrap/dist/css/bootstrap.min.css')
-	)
-);
-app.use(
 	'/jqureyLink',
 	express.static(path.join(__dirname, 'node_modules/jquery/dist/jquery.min.js'))
+);
+app.use(
+	'/bootstrap',
+	express.static(path.join(__dirname, 'node_modules/bootstrap/dist'))
 );
 
 // app.get('/', (req, res) => res.send('Hello World!'));
@@ -26,6 +33,20 @@ app.get('/', function(req, res) {
 
 app.get('/about', function(req, res) {
 	res.sendFile(__dirname + '/public/about.html');
+});
+
+app.get('/search=:term', function(req, res) {
+	console.log(client.TConsumerKey);
+	var term = req.params.term;
+	var params = {
+		q: term,
+		count: 1
+	};
+	client.get('search/tweets', params, function(err, tweet, twitterRes) {
+		if (!err) {
+			res.JSON(tweet);
+		}
+	});
 });
 
 // app.post()
